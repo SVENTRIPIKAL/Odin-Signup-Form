f8a3b58bcd71191ac594d7b5b5e888a2f7cf9688
sventripikal <180786857+SVENTRIPIKAL@users.noreply.github.com>
2025-12-19 16:13:20 -0600

    Update javascript for field validation

--- javascript/script.js
+++ javascript/script.js
@@ -1,17 +1,47 @@
-// assign element variables via ID
+// create & assign constants
+const invalidText = "Invalid Format.\nExample:";
+const phoneRegex = new RegExp(/(\d{10}|\d{3}-\d{3}-\d{4})/);
+const emailRegex = new RegExp(/[^\s\n@]+@[a-zA-Z]+\.(com|net|org|edu|gov)/);
+
+// assign element variables by ID
+var email = document.getElementById("email");
+var phoneNumber = document.getElementById("phone-number");
 var password = document.getElementById("password");
 var confirm_password = document.getElementById("confirm-password");
 
-// checks if values are same & sets custom validity text
-function validatePassword() {
-    if (password.value != confirm_password.value) {
-        confirm_password.setCustomValidity("Passwords Do Not Match");
-    } else {
-        confirm_password.setCustomValidity("");
+
+
+// checks if email is formatted correctly & sets validity text
+function validateEmail() {
+    if (emailRegex.test(email.value)) {
+        email.setCustomValidity("");
+    } else { 
+        email.setCustomValidity(`${invalidText} JohnDoe@gmail.com`);
     }
 }
 
-// runs validate password on every change in field
+// checks if phone number is formatted correctly & sets validity text
+function validatePhoneNumber() {
+    if (phoneRegex.test(phoneNumber.value)) {
+        phoneNumber.setCustomValidity("");
+    } else {
+        phoneNumber.setCustomValidity(`${invalidText} 123-456-7890 or 1234567890`);
+    }
+}
+
+// checks if passwords match & sets validity text
+function validatePassword() {
+    if (password.value == confirm_password.value) {
+        confirm_password.setCustomValidity("");
+    } else {
+        confirm_password.setCustomValidity("Passwords Do Not Match.");
+    }
+}
+
+
+
+// validates fields on every key release or input change
+email.onkeyup = validateEmail;
+phoneNumber.onkeyup = validatePhoneNumber;
 password.onchange = validatePassword;
-// runs validate password on every key release in field
 confirm_password.onkeyup = validatePassword;
